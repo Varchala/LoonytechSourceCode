7135c936ae8318c263ed80f6caaaca118d15f82d
varchala ganugapati <varchala@lincolner.com>
2017-08-27 12:47:11 +0530

    team update

--- mail/email.php
+++ mail/email.php
@@ -9,7 +9,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'teamloonytech@gmail.com';                 // SMTP username
-$mail->Password = 'Aknuv123!';                           // SMTP password
+$mail->Password = '';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
